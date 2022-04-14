39de613aaa503ee2a280f1d75fd75babcb04c6e6
Vitaly Gavrilov <vital.gavrilov1983@gmail.com>
2022-04-14 19:29:50 +0300

    HW03 SpringMain repaired

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -20,10 +20,3 @@ VALUES (100000, '2020-01-28 10:00:00', 'User Завтрак', 501),
        (100001, '2020-12-25 10:00:00', 'Admin Завтрак', 1000),
        (100001, '2020-12-31 13:00:00', 'Admin Обед', 500),
        (100001, '2020-12-31 20:00:00', 'Admin Ужин', 410);
-
-/*public static final Meal userMeal1 = new Meal(USER_MEAL1_ID, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "User Завтрак", 501);
-public static final Meal userMeal2 = new Meal(USER_MEAL2_ID, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "User Обед", 1000);
-public static final Meal userMeal3 = new Meal(USER_MEAL3_ID, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "User Ужин", 500);
-public static final Meal adminMeal1 = new Meal(ADMIN_MEAL1_ID, LocalDateTime.of(2020, Month.DECEMBER, 25, 10, 0), "Admin Завтрак", 1000);
-public static final Meal adminMeal2 = new Meal(ADMIN_MEAL2_ID, LocalDateTime.of(2020, Month.DECEMBER, 31, 13, 0), "Admin Обед", 500);
-public static final Meal adminMeal3 = new Meal(ADMIN_MEAL3_ID, LocalDateTime.of(2020, Month.DECEMBER, 31, 20, 0), "Admin Ужин", 410);*/
--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -4,6 +4,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.inmemory.InMemoryMealRepository;
+import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
 import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
@@ -14,10 +16,11 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
+//@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 automatic resource management (ARM)
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app-inmemory.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
