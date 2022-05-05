8d6f3f32db4c05278a1b0c4621feaab3acd9b951
Vitaly Gavrilov <vital.gavrilov1983@gmail.com>
2022-05-05 23:36:36 +0300

    @ActiveProfiles({"postgres", "datajpa"})

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -31,11 +31,11 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml",   "classpath:spring/envConfig.xml"
+        "classpath:spring/spring-db.xml"
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
+@ActiveProfiles({"postgres", "datajpa"})
 public class MealServiceTest {
     private static final Logger log = getLogger("result");
 
