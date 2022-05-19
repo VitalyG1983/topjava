92220b2be46530d2e2d084c0f3365c0223a01a71
Vitaly Gavrilov <vital.gavrilov1983@gmail.com>
2022-05-19 16:08:14 +0300

    HW06 Add AbstractMealController

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -1,8 +1,5 @@
-package ru.javawebinar.topjava.web;
+package ru.javawebinar.topjava.web.meal;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -11,9 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
@@ -28,22 +25,18 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
 @Controller
 @RequestMapping(value = "/meals")
-public class JspMealController {
-    private static final Logger log = LoggerFactory.getLogger(ru.javawebinar.topjava.web.JspMealController.class);
-
-    @Autowired
-    private MealService service;
+public class JspMealController extends AbstractMealController{
 
     @GetMapping
-    public String getMeals(Model model) {
-        log.info("getMeals");
+    public String getAll(Model model) {
+        log.info("getAll meals started");
         model.addAttribute("meals", MealsUtil.getTos(service.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay()));
         return "meals";
     }
 
     @GetMapping({"/update", "/create"})
     public String getForSave(Model model, @RequestParam(required = false) Integer id) {
-        log.info("getForSave started");
+        log.info("getForSave meal started");
         final Meal meal = id == null ?
                 new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
                 service.get(id, SecurityUtil.authUserId());
@@ -52,15 +45,15 @@ public class JspMealController {
     }
 
     @GetMapping(path = "/delete", params = {"id"})
-    public String deleteMeal(Integer id) {
-        log.info("deleteMeal started");
+    public String delete(Integer id) {
+        log.info("delete meal started");
         service.delete(id, SecurityUtil.authUserId());
         return "redirect:/meals";
     }
 
     @GetMapping("/filter")
-    public String filterMeals(Model model, HttpServletRequest request) {
-        log.info("filterMeals started");
+    public String filter(Model model, HttpServletRequest request) {
+        log.info("filter meals started");
         List<Meal> mealsDateFiltered = service.getBetweenInclusive(parseLocalDate(request.getParameter("startDate")),
                 parseLocalDate(request.getParameter("endDate")), SecurityUtil.authUserId());
         List<MealTo> mealTo = MealsUtil.getFilteredTos(mealsDateFiltered, SecurityUtil.authUserCaloriesPerDay(),
@@ -70,7 +63,7 @@ public class JspMealController {
     }
 
     @PostMapping
-    public String saveMeal(HttpServletRequest request) {
+    public String save(HttpServletRequest request) {
         Meal meal = new Meal(
                 LocalDateTime.parse(request.getParameter("dateTime")),
                 request.getParameter("description"),
