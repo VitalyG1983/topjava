60fa7131540567d0d249c36d30e9add8bdcaa8eb
Vitaly Gavrilov <vital.gavrilov1983@gmail.com>
2022-06-28 17:36:25 +0300

    HW10 Add custom UserValidator for check dublicate User email

--- src/main/java/ru/javawebinar/topjava/util/UserUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserUtil.java
@@ -13,6 +13,10 @@ public class UserUtil {
         return new User(null, userTo.getName(), userTo.getEmail().toLowerCase(), userTo.getPassword(), userTo.getCaloriesPerDay(), Role.USER);
     }
 
+    public static User createFromTo(UserTo userTo) {
+        return new User(userTo.getId() != null ? userTo.getId() : null, userTo.getName(), userTo.getEmail().toLowerCase(), userTo.getPassword(), userTo.getCaloriesPerDay(), Role.USER);
+    }
+
     public static UserTo asTo(User user) {
         return new UserTo(user.getId(), user.getName(), user.getEmail(), user.getPassword(), user.getCaloriesPerDay());
     }
---
+++ src/main/java/ru/javawebinar/topjava/web/UserValidator.java
@@ -0,0 +1,59 @@
+package ru.javawebinar.topjava.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.to.UserTo;
+import ru.javawebinar.topjava.util.UserUtil;
+
+import java.util.Locale;
+
+import static ru.javawebinar.topjava.util.exception.ErrorType.VALIDATION_ERROR;
+
+public class UserValidator implements Validator {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    MessageSource messageSource;
+
+    @Override
+    public boolean supports(Class clazz) {
+        return UserTo.class.isAssignableFrom(clazz) || User.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        User user;
+        User userByEmail = null;
+        if (target.getClass().equals(UserTo.class)) {
+            user = UserUtil.createFromTo((UserTo) target);
+        } else {
+            user = (User) target;
+        }
+        if (user.isNew()) {
+            try {
+                userByEmail = userService.getByEmail(user.getEmail());
+            } catch (Exception e) {
+                //e.printStackTrace();
+            }
+            //   update
+        } else try {
+            String newEmail = user.getEmail();
+            String oldEmail = userService.get(user.getId()).getEmail();
+            if (!oldEmail.equals(newEmail)) {
+                userByEmail = userService.getByEmail(newEmail);
+            }
+        } catch (Exception e) {
+            //e.printStackTrace();
+        }
+        if (userByEmail != null) {
+            errors.rejectValue("email", String.valueOf(VALIDATION_ERROR),
+                    messageSource.getMessage("user.doublicateEmail", new Object[]{}, Locale.getDefault()));
+        }
+    }
+}
